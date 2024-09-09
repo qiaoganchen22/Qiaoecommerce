b39e5b74bfb13fb4bd9a740d58229f352e97e8e0
qiaogan <qiaoganchen@gmail.com>
2024-09-09 17:48:25 -0400

    updated api

--- e-commerce/e-commerce/src/apis/api.js
+++ e-commerce/e-commerce/src/apis/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://e-commerce-rnms.onrender.com",
+    baseUrl: "https://qiaoecommerce.onrender.com",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().user.token;
       headers.set("Content-Type", "application/json");
